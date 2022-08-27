c112ebc108c146428bc820c35abd12f21a194878
erik7z_digis <e.nguita@do-it.co>
2022-08-27 13:34:27 +0700

    - updated docker-compose instruction

---
+++ nginx/build.sh
@@ -0,0 +1,1 @@
+ docker build -t seacontact-nginx . && docker tag seacontact-nginx zzzloy777/sc-nginx:latest && docker push zzzloy777/sc-nginx:latest
--- nginx/nginx-entrypoint.sh
+++ nginx/nginx-entrypoint.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env sh
 set -eu
 
-echo "#### nginx-entrypoint.sh >>>> replacing nginx config variables >>>>"
-
+echo "# nginx-entrypoint.sh > replacing nginx config variables >" "$NGINX_HOST" "$SSL_CERT_HOST" "$NGINX_PHP_SOURCE"
 envsubst '${NGINX_HOST} ${SSL_CERT_HOST} ${NGINX_PHP_SOURCE}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
-echo "#### nginx-entrypoint.sh >>>> completed replacing variables >>>> ready to start >>>>"
+echo "# nginx-entrypoint.sh > completed replacing variables > ready to start >"
 
 exec "$@"
