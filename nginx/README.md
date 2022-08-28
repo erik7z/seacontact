eab7ce82c0b651369a5ff71caa3676cc754ed721
erik7z_digis <e.nguita@do-it.co>
2022-08-28 18:03:02 +0700

    - updated deployment instruction for seacontact (in progress)

---
+++ nginx/README.md
@@ -0,0 +1,3 @@
+1. check envs in nginx.conf.template
+2. add same envs to Dockerfile
+3. provide values during deployment (k8/docker)
--- nginx/nginx-entrypoint.sh
+++ nginx/nginx-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 set -eu
 
-echo "# nginx-entrypoint.sh > replacing nginx config variables >" "$NGINX_HOST" "$SSL_CERT_HOST" "$NGINX_PHP_SOURCE"
-envsubst '${NGINX_HOST} ${SSL_CERT_HOST} ${NGINX_PHP_SOURCE}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+echo "# nginx-entrypoint.sh > replacing nginx config variables >" "$NGINX_HOST" "$SSL_CERT_HOST" "$NGINX_PHP_SOURCE" "$PHP_FPM_HOST"
+envsubst '${NGINX_HOST} ${SSL_CERT_HOST} ${NGINX_PHP_SOURCE} ${PHP_FPM_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 echo "# nginx-entrypoint.sh > completed replacing variables > ready to start >"
 
 exec "$@"
